be7cf20be6bcbdfcc4f193d6224a262b0826a24c
Trevor McGill <trevor.mcgill@icloud.com>
2023-08-02 06:41:34 -0500

    implemented asset caching with StaticWhileRevalidate

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  () => true,
+  new StaleWhileRevalidate({
+  cacheName: "static-resources",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+}));
